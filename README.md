d8ea13607631655a45650397f0b2f2460670e3f6
tGulevich <t.ananich@gmail.com>
2021-05-24 10:01:01 +0300

    set up environment

--- README.md
+++ README.md
@@ -1,1 +1,5 @@
 # github-users
+
+This is the test task for Startup Summer 2021 in Paralect.
+
+[Description of task] (https://startup-summer-task.paralect.com/)
---
+++ src/App.js
@@ -0,0 +1,11 @@
+import './App.scss';
+
+function App() {
+  return (
+    <div className="App">
+      <h1>GitHub User Search</h1>
+    </div>
+  );
+}
+
+export default App;
